b429dd0c63a0bd4d9849a4b466a39c421fa8452f
Pablo Gonzalez <pablogonzaleztemes@gmail.com>
2026-03-23 14:20:52 +0100

    style(TablaAsignacion): :art: remove unnecesary comments

--- src/main/java/edu/teamrocket/dni/TablaAsignacion.java
+++ src/main/java/edu/teamrocket/dni/TablaAsignacion.java
@@ -38,54 +38,6 @@ class TablaAsignacion {
 }
 
 /*
- * class TablaAsignacion:
- * def __init__(self):
- * self.tabla = (
- * "T",
- * "R",
- * "W",
- * "A",
- * "G",
- * "M",
- * "Y",
- * "F",
- * "P",
- * "D",
- * "X",
- * "B",
- * "N",
- * "J",
- * "Z",
- * "S",
- * "Q",
- * "V",
- * "H",
- * "L",
- * "C",
- * "K",
- * "E",
- * )
- * 
- * def getTabla(self):
- * return self.tabla
- * 
- * def getLetra(self, posicion):
- * try:
- * return self.tabla[posicion]
- * except IndexError:
- * return "Posicion letra fuera de rango"
- * 
- * def calcularLetra(self, cifras):
- * return TablaAsignacion.getTabla(self)[
- * int(cifras) % len(TablaAsignacion.getTabla(self))
- * ]
- * 
- * def getModulo(self):
- * return len(TablaAsignacion.getTabla(self))
- * 
- * def isLetraPermitida(self, letra):
- * return letra in TablaAsignacion.getTabla(self)
- * 
  * def __repr__(self):
  * return "Tabla de asignación"
  */
